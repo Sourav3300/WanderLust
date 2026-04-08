9c81801f4b8d281f0d58416f15094df1a97f47f7
Sourav3300 <sourov3300@gmail.com>
2026-04-08 12:24:10 +0200

    added

--- schema.js
+++ schema.js
@@ -1,24 +1,18 @@
 const joi = require("joi");
 
 module.exports.ListingSchema = joi.object({
-  listing : joi.object({
-    title : joi.string().required(),
-    description : joi.string().required(),
-    location : joi.string().required(),
-    country : joi.string().required(),
-    price : joi.number().required().min(0),
-    image: joi.object({
-      filename: joi.string().optional(),
-      url: joi.string().required()
-})
+  listing: joi.object({
+    title: joi.string().required(),
+    description: joi.string().required(),
+    location: joi.string().required(),
+    country: joi.string().required(),
+    price: joi.number().required().min(0)
   }).required()
-}
-)
+});
 
-module.exports.ReviewSchema =joi.object({
-  review : joi.object({
-   rating: joi.number().required().min(1).max(5),
-   comment : joi.string().required()
+module.exports.ReviewSchema = joi.object({
+  review: joi.object({
+    rating: joi.number().required().min(1).max(5),
+    comment: joi.string().required()
   }).required()
-  
-})
+});
